a7cf54e579d732cf95011bf0b0598d7e8e2b17fc
IT24102270 <it24102270@my.sliit.lk>
2025-08-20 12:46:50 +0530

    Project Initialization

---
+++ src/main/java/com/sliit/parking_reservation_and_management_system/ParkingReservationAndManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.parking_reservation_and_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingReservationAndManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingReservationAndManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sliit/parking_reservation_and_management_system/ParkingReservationAndManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.parking_reservation_and_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingReservationAndManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
